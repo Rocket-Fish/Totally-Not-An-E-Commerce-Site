704ccb64f697c6cf5e3a53ce8841f76e31a04ebb
Rocket-Fish <rocket.fish@mail.com>
2019-07-15 16:27:07 -0400

    modified forms to use the newly added form input component and custom button for submission

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
 import './sign-in.styles.sass'
 
 class SignIn extends React.Component {
@@ -30,20 +33,21 @@ class SignIn extends React.Component {
 				<h2> I already have an account</h2>
 				<span>Sign in with your email and password</span>
 				<form onSubmit={this.handleSubmit}>
-					<input name='email' 
+					<FormInput 
+						name='email' 
 						type='email' 
+						label='email'
 						value={this.state.email} 
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
 						required />
-					<label> Email </label>
-					<input name='password' 
+					<FormInput 
+						name='password' 
 						type='password' 
+						label='password'
 					  value={this.state.password} 
-						onChange={this.handleChange}
+						handleChange={this.handleChange}
 						required />
-					<label> Password </label>
-
-					<input type='submit' value='Submit Form' />
+					<CustomButton type='submit'> Sign in </CustomButton>
 				</form>
 			</div>
 		)
