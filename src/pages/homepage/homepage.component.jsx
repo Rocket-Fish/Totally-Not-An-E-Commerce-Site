08cca5aba392327043b2764098f2bf2213a9a7c1
Rocket-Fish <rocket.fish@mail.com>
2019-07-15 09:53:53 -0400

    split homepage elements into its own individual components
    refactored homepage component to export default

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.compontent'
+
+import './homepage.styles.sass'
+
+const Homepage = () => (
+	<div className = 'homepage'> 
+		<Directory />
+	</div>
+)
+
+export default Homepage
