d9810d1533126c4c6c96f75fc1b7ad9f380ea816
Rocket-Fish <rocket.fish@mail.com>
2019-07-17 17:02:32 -0400

    currentUser in state will now store an object containing the login information and login data of the user retrieved from the database

--- src/App.js
+++ src/App.js
@@ -13,16 +13,30 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      currentUser:null
+      currentUser:null, 
+      memes: 'meme value'
     }
   }
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      this.setState({currentUser: user})
-      createUserProfileDocument(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // if userAuth is not null (i.e. they are signed in)
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id, 
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      else {
+        this.setState({currentUser:null})
+      }
     })
   }
 
