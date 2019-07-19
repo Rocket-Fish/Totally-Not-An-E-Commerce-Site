78cb1cf434055e6a18c14ac3ea10b8fb17f71f9f
Rocket-Fish <rocket.fish@mail.com>
2019-07-19 09:05:56 -0400

    add user action type, so naming is consistent.

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,4 @@
+import {UserActionTypes} from './user.types'
 const INITIAL_STATE = {
 	currentUser:null
 }
@@ -11,7 +12,7 @@ action is literally an object in the following format as specified in user.actio
 */
 const userReducer = (state = INITIAL_STATE, action) =>  {
 	switch (action.type) {
-		case 'SET_CURRENT_USER':
+		case UserActionTypes.SET_CURRENT_USER:
 			return {
 				...state, 
 				currentUser: action.payload
