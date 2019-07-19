65dbaa837e4745c0b85beee4da907e3d3dc8f3f2
Rocket-Fish <rocket.fish@mail.com>
2019-07-19 08:06:59 -0400

    added root reducer, user reducer

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,27 @@
+const INITIAL_STATE = {
+	currentUser:null
+}
+
+/*
+action is literally an object in the following format
+{
+	type: 
+	payload: 
+}
+*/
+const userReducer = (state = INITIAL_STATE, action) =>  {
+	switch (action.type) {
+		case 'SET_CURRENT_User':
+			return {
+				...state, 
+				currentUser: action.payload
+			} 
+
+		// note that every single action gets passed through every reducer
+		// so if no action type is found in this reducer, just return nothing
+		default: 
+			return state
+	}
+}
+
+export default userReducer
