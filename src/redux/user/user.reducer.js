a0537df55d0031c0d57982834c56a706e40ec055
Rocket-Fish <rocket.fish@mail.com>
2019-07-19 08:56:45 -0400

    SET_CURRENT_User typo --> SET_CURRENT_USER

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -11,7 +11,7 @@ action is literally an object in the following format as specified in user.actio
 */
 const userReducer = (state = INITIAL_STATE, action) =>  {
 	switch (action.type) {
-		case 'SET_CURRENT_User':
+		case 'SET_CURRENT_USER':
 			return {
 				...state, 
 				currentUser: action.payload
