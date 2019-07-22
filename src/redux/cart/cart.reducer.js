3a86f49e375a60c776e79db907ed52c40e35cbad
Rocket-Fish <rocket.fish@mail.com>
2019-07-22 13:41:14 -0400

    checkout component can now increase and decrease the items in the checkout or remove the item if the item count is being lowered under 1

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart, deleteItemEntryFromCart} from './cart.utils'
 
 const INITIAL_STATE = {
 	hidden: true, 
@@ -21,7 +21,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case CartActionTypes.CLEAR_ITEM_FROM_CART: 
 			return {
 				...state, 
-				cartItems: state.cartItems.filter(cartItems => cartItems.id !== action.payload.id)
+				cartItems: deleteItemEntryFromCart(state.cartItems, action.payload)
+			}
+		case CartActionTypes.REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: removeItemFromCart(state.cartItems, action.payload)
 			}
 		default: 
 			return state
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,6 +11,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	else {
 		return [...cartItems, {...cartItemToAdd, quantity: 1}]
 	}
-
-
 }  
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id)
+	if(existingCartItem.quantity === 1) {
+		// case only 1 remain, reducing it to 0 = remove the item
+		return deleteItemEntryFromCart(cartItems, cartItemToRemove)
+	}
+	return cartItems.map (
+		cartItem => 
+		// if the id matches, reduce the quantity by 0, else just return the item without change
+		cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem
+	)
+}
+
+const deleteItemEntryFromCart = (cartItems, cartItemToDelete) => {
+	return cartItems.filter(cartItem => cartItem.id !== cartItemToDelete.id)
+}
+
+export {deleteItemEntryFromCart}
